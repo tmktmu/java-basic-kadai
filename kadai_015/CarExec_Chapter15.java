80877fb8d04976948dd4a86ac191f85bdf1c7ff6
tmktmu <161949575+tmktmu@users.noreply.github.com>
2025-03-30 19:09:51 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,15 @@
+package kadai_015;
+
+//実行用クラス
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		//Car_Chapter15クラスのインスタンス化
+		Car_Chapter15 Car_Chapter = new Car_Chapter15();
+		Car_Chapter.gearChange(3);
+		Car_Chapter.run();
+
+	}
+
+}
