492b2d2f5c38a69a9bfcb6c2d62f4a6a89d1bab9
tmktmu <tmktmu@icloud.com>
2025-04-12 13:26:30 +0900

    [update]模範解答への修正
    refs #001 コード数が少なくなるよう修正しました

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,38 +1,29 @@
 package kadai_015;
 
-//設計図クラス
 public class Car_Chapter15 {
-	
-	//インスタンスフィールド
-	private int gear;
-	private int speed;
-	
-	//インスタンスメソッド
+	// フィールド
+	private int gear = 1; //ギア
+	private int speed = 10; //速度
+
+	//【メソッド】ギアの値により速度を変える
 	public void gearChange(int afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
 		this.gear = afterGear;
-		switch(this.gear) {
-		case 1 -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		case 2 -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		case 3 -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		case 4 -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		case 5 -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		default -> System.out.println("ギアが1から"+this.gear+"に切り替えました");
-		}
-	}
-	
-	public void run() {
-		switch(this.gear) {
-		case 1 -> this.speed=10;
-		case 2 -> this.speed=20;
-		case 3 -> this.speed=30;
-		case 4 -> this.speed=40;
-		case 5 -> this.speed=50;
-		default -> this.speed=10;
-		}
-		
-		System.out.println("速度は時速"+this.speed+"kmです");
-	}
-	
+
+		// ギアに応じた速度を取得
+		this.speed = switch (this.gear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		};
 	}
 
+	//【メソッド】ギアチェンジ後の速度を表示する
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");
+	}
+}
 
